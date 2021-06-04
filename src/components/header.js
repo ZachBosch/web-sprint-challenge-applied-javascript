29cb24fd84bd8b7cba7da36add69c70b24743b54
Zulu Bravo <zulubravo@zulus-mbp.myfiosgateway.com>
2021-06-04 15:11:44 -0400

    finished

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ const Header = (title, date, temp) => {
   const heading = document.createElement('h1')
   const tempSpan = document.createElement('span')
 
-  ////////////// Setting classes and text /////////////////
+  ////////////// Setting classes, text, attrs /////////////////
   headerDiv.classList.add('header')
   dateSpan.classList.add('date')
   tempSpan.classList.add('temp')
@@ -37,12 +37,15 @@ const Header = (title, date, temp) => {
   return headerDiv
 }
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+  // const hideInstructions = () => {
+  //   // TASK 2
+  //   // ---------------------
+  //   // Implement this function taking a css selector as its only argument.
+  //   // It should create a header using the Header component above, passing arguments of your choosing.
+  //   // It should append the header to the element in the DOM that matches the given selector.
+  //   //
+  // }
+
   //////// Get date the fancy way /////////////
   var newDate = new Date();
   var today = newDate.toDateString()
